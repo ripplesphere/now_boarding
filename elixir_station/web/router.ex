656557e2bfec283b737292e5dde615e0ec4e12a2
George Mills <george.r.mills@gmail.com>
2017-03-05 14:51:27 -0500

    Disabled html version of server.

--- elixir_station/web/router.ex
+++ elixir_station/web/router.ex
@@ -17,7 +17,6 @@ defmodule ElixirStation.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/departures", DeparturesController
   end
 
   # Other scopes may use custom stacks.
