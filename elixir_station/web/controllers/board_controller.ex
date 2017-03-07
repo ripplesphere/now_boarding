0f070eb869644c6865ee8142025c3789908030f5
George Mills <george.r.mills@gmail.com>
2017-03-07 16:27:44 -0500

    Add functionality to handle specifying station.

--- elixir_station/web/controllers/board_controller.ex
+++ elixir_station/web/controllers/board_controller.ex
@@ -3,13 +3,19 @@ defmodule ElixirStation.BoardController do
 
   alias ElixirStation.Departures
 
-   def index(conn, _params) do
+   def index(conn, %{"station" => station} = params) do
 
       # Some of this should probably be done in the Departures model but I have yet
       #   to figure that out.
-      departures = Repo.all(Departures) |> Enum.map(&(Map.take(&1, 
-            [:t_stamp, :origin, :trip, :destination, :scheduled_time,
-             :lateness, :track, :status])))
+      for_station = case station do
+         "south" ->
+            "South Station"
+         "north" -> 
+            "North Station"
+      end
+      query = from "departures", where: [origin: ^for_station], order_by: [:scheduled_time],
+               select: [:t_stamp, :trip, :destination, :scheduled_time, :lateness, :track, :status]
+      departures = Repo.all(query)
       # And one day I will learn how to do this all in one line of code, until then....
       ecto_time = Enum.map(departures, fn(x) -> DateTime.from_unix!(String.to_integer(x[:scheduled_time])) end)
       h = ecto_time |> Enum.map( fn(x) -> "#{x.hour}" end)
@@ -19,10 +25,11 @@ defmodule ElixirStation.BoardController do
       h12s = h |> Enum.map( fn(x) -> cond do String.to_integer(x) > 12 -> " PM"
                                              true -> " AM" end end)
       m2 = m |> Enum.map( fn(x) -> cond do x == "0" -> ":00"
+                                             String.to_integer(x) < 10 -> ":0" <> x
                                              true -> ":" <> x end end)
       zl = List.zip([h12, m2, h12s])
       ft = zl |> Enum.map( fn(x) -> List.to_string(Tuple.to_list(x)) end)
-      d_limited = Enum.map(departures, fn(x) -> [x.origin, x.trip, x.destination, x.lateness, x.track, x.status] end)
+      d_limited = Enum.map(departures, fn(x) -> [x.trip, x.destination, x.lateness, x.track, x.status] end)
       d_tmp = List.zip([d_limited, ft])
       d_list = d_tmp |> Enum.map( fn(x) -> Tuple.to_list(x) end )
       d_ready = d_list |> Enum.map( fn(x) -> List.flatten(x) end )
